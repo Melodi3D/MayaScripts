44664c2b1d2c7c8b816f9ad5b2ce434dae20a87b
Melodi <83541357+Melodi3D@users.noreply.github.com>
2026-08-08 08:14:15 -0400

    Add files via upload

---
+++ practicetools/landmark.py
@@ -0,0 +1,35 @@
+from maya import cmds
+
+def create_landmark(colors):
+    #user selects faces
+
+    selection = cmds.ls(sl=True, flatten=True)
+
+
+    #selects selection
+
+    cmds.select(selection)
+
+
+    #creates landmark shader with lambert material
+
+    landmark_shader = cmds.shadingNode(“lambert”, asShader=True)
+
+
+    #assigns shader 
+
+    cmds.hyperShade(assign=landmark_shader)
+
+
+
+
+
+    #creates landmark shader colors
+
+    cmds.setAttr(
+
+    f”{landmark_shader}.color”,
+
+    colors[0], colors[1], colors[2],
+
+    type=“double3”)
