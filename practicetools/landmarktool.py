7bffe8595de16809bf19d6a387340d34ab7e1509
Melodi <83541357+Melodi3D@users.noreply.github.com>
2026-08-12 05:07:45 -0400

    Update landmarktool.py

--- practicetools/landmarktool.py
+++ practicetools/landmarktool.py
@@ -11,19 +11,21 @@ def create_landmark(colors):
 
     selection = cmds.ls(sl=True, flatten=True)
 
+    # error handling due to lack of selection
+    if not selection:
+    raise RuntimeError("Error: Nothing is selected")
 
     #selects selection
 
     cmds.select(selection)
-
+    
 
     #creates landmark shader as a shader node, with lambert material
 
     landmark_shader = cmds.shadingNode("lambert", asShader=True)
 
 
-    #assigns shader to land_mark shader
-
+    #assigns shader to the selected faces
     cmds.hyperShade(assign=landmark_shader)
 
 
