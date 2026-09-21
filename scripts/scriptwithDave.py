b406be6d5dd165860e9c086c37f38726bf98c0d0
Melodi <83541357+Melodi3D@users.noreply.github.com>
2026-09-21 17:49:04 -0400

    Add files via upload

---
+++ scripts/scriptwithDave.py
@@ -0,0 +1,7 @@
+myVar = None
+
+print(type(myVar)) #prints the datatype of myVar
+
+print(isinstance(myVar, type(None))) #returns True/False if myVar is the same as type(None)
+
+print(myVar == None) # returns True/False if myVar == None datatype
